de8fa85efc6b8c17fa6e9a69a657611112518824
Olivier Richard <olivier.richard@imag.fr>
2021-01-11 21:49:59 +0100

    add init command and minimal state

---
+++ nixos_compose/commands/cmd_init.py
@@ -0,0 +1,72 @@
+import os
+import os.path as op
+import json
+import random
+
+from io import open
+
+import click
+
+from ..context import pass_context, on_finished
+from ..utils import copy_tree, copy_file
+
+EXAMPLES_PATH = op.abspath(op.join(op.dirname(__file__), "../..", "examples"))
+EXAMPLES = os.listdir(EXAMPLES_PATH)
+NXC_NIX_PATH = op.abspath(op.join(op.dirname(__file__), "../../nix"))
+
+
+@click.command("init")
+#@click.option("-f", "--force", is_flag=True, help="Overwrite existing env")
+@click.option(
+    "-e",
+    "--example",
+    default="test-vm.nix",
+    help="Use example",
+    show_default=True,
+    type=click.Choice(EXAMPLES),
+)
+@pass_context
+@on_finished(lambda ctx: ctx.state.dump())
+def cli(ctx, example):
+    """Initialize a new environment."""
+    
+    overwrite = False
+
+    example_path = op.abspath(op.join(EXAMPLES_PATH, example))
+    composition_path = op.abspath(op.join(ctx.envdir, example))
+
+    create = click.style('   create', fg="green")
+    click.echo("   " + create + "  " + ctx.envdir)
+    os.mkdir(ctx.envdir)
+    
+    
+    click.echo("   " + create + "  " + composition_path)
+    copy_file(
+        example_path,
+        composition_path,
+    )
+    #composition_path2 = op.abspath(op.join(ctx.envdir, "..", "composition.nix"))
+    #if composition_path != composition_path2:
+    #    os.symlink(composition_path, composition_path2)
+        
+    copy_tree(
+         NXC_NIX_PATH,
+         op.abspath(op.join(ctx.envdir, 'nix')),
+    )
+    
+    nxc_json = json.dumps({'envdir': ctx.envdir, 'composition': composition_path})
+    
+    #for nxc_json_dir in [op.abspath(op.join(ctx.envdir, '..')), ctx.envdir]:
+    nxc_json_file = op.abspath(op.join(ctx.envdir, "nxc.json"))
+
+    click.echo("   " + create + "  " + nxc_json_file)
+
+    with open(nxc_json_file, "w") as f:
+        f.write(nxc_json) 
+    os.symlink(nxc_json_file, op.abspath(op.join(ctx.envdir, "..", "nxc.json")))
+    
+    ctx.log(
+        "Initialized nixos-compose environment in %s", click.format_filename(ctx.envdir)
+    )
+
+    #ctx.init_env(env_name=env, env_id=env_id)
---
+++ nixos_compose/state.py
@@ -0,0 +1,33 @@
+import json
+import os.path as op
+import pprint
+
+from io import open
+from .utils import touch
+
+class State(dict):
+    DEFAULTS = {"built": False, "started": False}
+
+    def __init__(self, ctx, state_file):
+        dict.__init__(self, self.DEFAULTS)
+        self.ctx = ctx
+        self.state_file = state_file
+        self.load()
+
+    def load(self):
+        if op.isfile(self.state_file):
+            try:
+                with open(self.state_file, 'rt') as json_file:
+                    self.update(json.loads(json_file.read()))
+            except:
+                pass
+
+    def dump(self):
+        if op.isdir(self.ctx.envdir):
+            touch(self.state_file)
+            if op.isdir(op.dirname(self.state_file)):
+                with open(self.state_file, "w", encoding='utf8') as json_file:
+                    json_file.write(json.dumps(self, ensure_ascii=False))
+
+    def __str__(self):
+        return pprint.pprint(self)
