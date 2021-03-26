63fdd6b596a09bbcd59d4b3aaa2ca1a40fa00523
Olivier Richard <olivier.richard@imag.fr>
2021-03-26 13:08:31 +0100

    Init_command: complete flake and nur support

--- nixos_compose/commands/cmd_init.py
+++ nixos_compose/commands/cmd_init.py
@@ -15,33 +15,72 @@ EXAMPLES_PATH = op.abspath(op.join(op.dirname(__file__), "../..", "examples"))
 EXAMPLES = os.listdir(EXAMPLES_PATH)
 NXC_NIX_PATH = op.abspath(op.join(op.dirname(__file__), "../../nix"))
 
-FLAKE_TEMPLATE = """{
+FLAKE = """{
   description = "nixos-compose - composition to infrastructure";
-
-  outputs = { self, nixpkgs }:
+  $input_nur
+  outputs = { self, nixpkgs$nur_arg }:
     let
       system = "x86_64-linux";
 
-      composition = import ./$composition;
-
-      nixos_test = import ./nix/nixos-test.nix;
-      generate = import ./nix/generate.nix;
       flavours = import ./nix/flavours.nix;
-
+      $nur
     in {
-      packages.x86_64-linux = nixpkgs.lib.mapAttrs (name: flavour:
-        generate { inherit nixpkgs system flavour; } composition) flavours // {
-          nixos-test = nixos_test { inherit nixpkgs system; } composition;
-          nixos-test-driver =
-            (nixos_test { inherit nixpkgs system; } composition).driver;
-        };
-      defaultPackage.x86_64-linux = self.packages.x86_64-linux.$default_flavour;
+      packages.$${system} = nixpkgs.lib.mapAttrs (name: flavour:
+        (import ./nix/compose.nix) {
+          inherit nixpkgs system$extra_configurations flavour;
+        }) flavours;
+
+      defaultPackage.$${system} = self.packages.$${system}.$default_flavour;
+
     };
 }"""
+
+INPUT_NUR_FLAKE = """
+  inputs.nur.url = "github:nix-community/NUR";
+"""
+
+NUR_FLAKE = """
+      # to allow NUR modules access
+      nur-no-pkgs =
+        import nur { nurpkgs = import nixpkgs { inherit system; }; };
+      extraConfigurations = [
+        # add nur attribute to pkgs
+        { nixpkgs.overlays = [ nur.overlay ]; }
+        # import NUR modules like this:
+        # nur-no-pkgs.repos.alice.modules.foo
+      ];
+"""
 
 DEFAULT_COMPAT_FLAKE = """(import (builtins.fetchTarball https://github.com/edolstra/flake-compat/archive/master.tar.gz) {
   src = ./.;
 }).defaultNix"""
+
+DEFAULT = """{ nixpkgs ? <nixpkgs>, system ? builtins.currentSystem, flavour ? "$default_flavour"
+}:
+$nur
+(import ./nix/compose.nix) {
+  inherit nixpkgs system$extra_configurations flavour;
+}
+"""
+
+DEFAULT_NUR = """
+let
+  nur = builtins.fetchTarball
+    "https://github.com/nix-community/NUR/archive/master.tar.gz";
+  # to allow NUR modules access
+  nur-no-pkgs = import nur { nurpkgs = import nixpkgs { inherit system; }; };
+  extraConfigurations = [
+    {
+      # add nur attribute to pkgs
+      nixpkgs.config.packageOverrides = pkgs: {
+         nur = (import nur) { inherit pkgs; };
+      };
+    }
+    # import NUR modules like this:
+    # nur-no-pkgs.repos.alice.modules.foo
+  ];
+in
+"""
 
 
 @click.command("init")
@@ -65,9 +104,10 @@ DEFAULT_COMPAT_FLAKE = """(import (builtins.fetchTarball https://github.com/edol
 @click.option(
     "--flake", is_flag=True, help="Add flake.nix and default.nix for compatibility."
 )
+@click.option("--nur", is_flag=True, help="Add Nix User Repository (NUR) access.")
 @pass_context
 @on_finished(lambda ctx: ctx.state.dump())
-def cli(ctx, example, no_symlink, disable_detection, flake):
+def cli(ctx, example, no_symlink, disable_detection, flake, nur):
     """Initialize a new environment."""
 
     nxc_json_file = op.abspath(op.join(ctx.envdir, "nxc.json"))
@@ -78,9 +118,9 @@ def cli(ctx, example, no_symlink, disable_detection, flake):
 
     else:
 
-        composition_file = example
+        composition_file = "composition.nix"
         composition_path = op.abspath(op.join(ctx.envdir, composition_file))
-
+        click.echo("\nCopy intial files: ")
         create = click.style("   create", fg="green")
         click.echo("   " + create + "  " + ctx.envdir)
         os.mkdir(ctx.envdir)
@@ -97,21 +137,38 @@ def cli(ctx, example, no_symlink, disable_detection, flake):
         if not disable_detection:
             platform_detection(ctx)
 
-        if flake:
-            click.echo("\nCreate files for flake support")
-            # determine default flavour
-            if ctx.platform:
-                default_flavour = ctx.platform.default_flavour
+        # determine default flavour
+        if ctx.platform:
+            default_flavour = ctx.platform.default_flavour
+        else:
+            default_flavour = "nixos-test"
+        click.echo("      default flavour: " + click.style(default_flavour, fg="green"))
+
+        nur_str = ""
+        nur_arg = ""
+        extra_configurations = ""
+        input_nur = ""
+        if nur:
+            extra_configurations = " extraConfigurations"
+            if flake:
+                nur_str = NUR_FLAKE
+                nur_arg = ", nur"
+                input_nur = INPUT_NUR_FLAKE
             else:
-                default_flavour = "nixos-test"
+                nur_str = DEFAULT_NUR
 
-            click.echo(
-                "      default flavour: " + click.style(default_flavour, fg="green")
-            )
+            click.echo("      add nur:" + click.style("done", fg="green"))
 
-            t = Template(FLAKE_TEMPLATE)
+        if flake:
+            click.echo("\nCreate files for flake support:")
+
+            t = Template(FLAKE)
             flake_nix = t.substitute(
-                composition=composition_file, default_flavour=default_flavour
+                default_flavour=default_flavour,
+                nur=nur_str,
+                nur_arg=nur_arg,
+                input_nur=input_nur,
+                extra_configurations=extra_configurations,
             )
 
             click.echo("   " + create + "  flake.nix")
@@ -121,6 +178,17 @@ def cli(ctx, example, no_symlink, disable_detection, flake):
                 click.echo("   " + create + "  default.nix")
                 with open(op.abspath(op.join(ctx.envdir, "default.nix")), "w") as f:
                     f.write(DEFAULT_COMPAT_FLAKE)
+        else:
+            click.echo("\nCreate default file:")
+            click.echo("   " + create + "  default.nix")
+            t = Template(DEFAULT)
+            default_nix = t.substitute(
+                default_flavour=default_flavour,
+                nur=nur_str,
+                extra_configurations=extra_configurations,
+            )
+            with open(op.abspath(op.join(ctx.envdir, "default.nix")), "w") as f:
+                f.write(default_nix)
 
         nxc_json = {"composition": composition_file, "flake": flake}
         nxc_json_str = json.dumps(nxc_json)
