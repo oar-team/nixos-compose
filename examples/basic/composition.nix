ba095dbf4b6016b3e893e74673a44efd38d85476
Olivier Richard <olivier.richard@imag.fr>
2021-10-12 17:01:58 +0200

    fix basic example

--- examples/basic/composition.nix
+++ examples/basic/composition.nix
@@ -1,15 +1,10 @@
 { pkgs, ... }: {
   nodes = {
-    foo = { pkgs, lib, ... }: {
-
-      # services.sshd.enable = true;
-      # networking.firewall.allowedTCPPorts = [ 80 ];
-
-      # users.users.root.password = "nixos";
-      # services.openssh.permitRootLogin = lib.mkDefault "yes";
-      # services.getty.autologinUser = lib.mkDefault "root";
-
-    };
+    foo = { pkgs, ... }:
+      {
+        # add needed package
+        # environment.systemPackages = with pkgs; [ socat ];
+      };
   };
   testScript = ''
     foo.succeed("true")
--- examples/basic/flake.nix
+++ examples/basic/flake.nix
@@ -1,25 +1,17 @@
 {
   description = "nixos-compose - composition to infrastructure";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs?rev=7e9b0dff974c89e070da1ad85713ff3c20b0ca97";
 
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
 
-      composition = import ./composition.nix;
-
-      nixos_test = import ./nix/nixos-test.nix;
-      generate = import ./nix/generate.nix;
-      compose = import ./nix/compose.nix;
       flavours = import ./nix/flavours.nix;
 
     in {
-      packages.x86_64-linux = nixpkgs.lib.mapAttrs (name: flavour:
-        compose { inherit nixpkgs system flavour composition; }) flavours // {
-          nixos-test = nixos_test { inherit nixpkgs system; } composition;
-          nixos-test-driver =
-            (nixos_test { inherit nixpkgs system; } composition).driver;
-        };
-      defaultPackage.x86_64-linux = self.packages.x86_64-linux.nixos-test;
+      packages.${system} =
+        (import ./nix/compose.nix) { inherit nixpkgs system flavours; };
+
+      defaultPackage.${system} =
+        self.packages.${system}."composition::nixos-test";
     };
 }
