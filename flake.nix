089378f157f5baf111156703a72d57d6999bb1fc
Olivier Richard <olivier.richard@imag.fr>
2021-10-13 17:45:37 +0200

    add flake.nix/flake.lock for the project

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "nixos-compose";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        #customOverrides = self: super: {
+        # Overrides go here
+        #};
+
+        app = pkgs.poetry2nix.mkPoetryApplication {
+          projectDir = ./.;
+          #overrides =
+          #  [ pkgs.poetry2nix.defaultPoetryOverrides customOverrides ];
+          propagatedBuildInputs = [ pkgs.openssh ];
+        };
+
+        packageName = "nixos-compose";
+      in {
+        packages.${packageName} = app;
+
+        defaultPackage = self.packages.${system}.${packageName};
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ poetry ];
+          inputsFrom = builtins.attrValues self.packages.${system};
+        };
+      });
+}
