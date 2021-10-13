70eb98b59dbc305006cb2d726244baec7e740da6
Olivier Richard <olivier.richard@imag.fr>
2021-10-13 10:19:49 +0200

    init: only flake support, setting default flavour have no effect up to now

--- nixos_compose/commands/cmd_init.py
+++ nixos_compose/commands/cmd_init.py
@@ -1,7 +1,7 @@
 import os
 import os.path as op
 import json
-from string import Template
+
 import sys
 
 from io import open
@@ -9,82 +9,14 @@ from io import open
 import click
 
 from ..context import pass_context
-from ..platform import platform_detection
+
+# from ..platform import platform_detection
 from ..utils import copy_tree, copy_file
 
 EXAMPLES_PATH = op.abspath(op.join(op.dirname(__file__), "../..", "examples"))
 EXAMPLES = os.listdir(EXAMPLES_PATH)
 EXAMPLES.remove("flake.lock")
 NXC_NIX_PATH = op.abspath(op.join(op.dirname(__file__), "../../nix"))
-
-FLAKE = """{
-  description = "nixos-compose - composition to infrastructure";
-  $input_nur
-  outputs = { self, nixpkgs$nur_arg }:
-    let
-      system = "x86_64-linux";
-
-      flavours = import ./nix/flavours.nix;
-      $nur
-    in {
-        packages.$${system} =
-          (import ./nix/compose.nix) { inherit nixpkgs system flavours; };
-
-        defaultPackage.$${system} =
-          self.packages.$${system}."composition::$default_flavour";
-    };
-}"""
-
-INPUT_NUR_FLAKE = """
-  inputs.NUR.url = "github:nix-community/NUR";
-  #inputs.alice.url = "path:/home/some_path/nur-alice";
-"""
-
-NUR_FLAKE = """
-      nur = import ./nix/nur.nix {
-        inherit nixpkgs system NUR;
-        # for repo override if needed
-        #repoOverrides = { inherit alice; };
-      };
-
-      extraConfigurations = [
-        # add nur attribute to pkgs
-        { nixpkgs.overlays = [ nur.overlay ]; }
-        #nur.repos.alice.modules.foo
-      ];
-"""
-
-DEFAULT_COMPAT_FLAKE = """(import (builtins.fetchTarball https://github.com/edolstra/flake-compat/archive/master.tar.gz) {
-  src = ./.;
-}).defaultNix"""
-
-DEFAULT = """{ nixpkgs ? <nixpkgs>, system ? builtins.currentSystem, flavour ? "$default_flavour"
-}:
-$nur
-(import ./nix/compose.nix) {
-  inherit nixpkgs system$extra_configurations flavour;
-}
-"""
-
-DEFAULT_NUR = """
-let
-  NUR = builtins.fetchTarball
-    "https://github.com/nix-community/NUR/archive/master.tar.gz";
-
-  nur = import ./nix/nur.nix {
-    inherit nixpkgs system NUR;
-    # for repo override if needed
-    #repoOverrides = { alice = /home/some_path/nur-alice; };
-  };
-
-  extraConfigurations = [
-    # add nur attribute to pkgs
-    { nixpkgs.overlays = [ nur.overlay ]; }
-    # import NUR modules like this:
-    #nur.repos.alice.modules.foo
-  ];
-in
-"""
 
 
 @click.command("init")
@@ -104,9 +36,6 @@ in
 )
 @click.option(
     "-n", "--disable-detection", is_flag=True, help="Disable platform detection."
-)
-@click.option(
-    "--flake", is_flag=True, help="Add flake.nix and default.nix for compatibility."
 )
 @click.option("--nur", is_flag=True, help="Add Nix User Repository (NUR) access.")
 @click.option(
@@ -135,7 +64,6 @@ def cli(
     example,
     no_symlink,
     disable_detection,
-    flake,
     nur,
     default_flavour,
     list_flavours,
@@ -159,6 +87,14 @@ def cli(
         print(json.dumps(description_flavours, indent=4))
         sys.exit(0)
 
+    if nur:
+        if example != "basic":
+            raise click.ClickException("nur option cannot be used with example")
+        else:
+            example = "basic-nur"
+
+    example_path = op.abspath(op.join(EXAMPLES_PATH, example))
+
     example_path = op.abspath(op.join(EXAMPLES_PATH, example))
     if op.isdir(example_path):
         copy_tree(example_path, ctx.envdir)
@@ -166,85 +102,15 @@ def cli(
             NXC_NIX_PATH, op.abspath(op.join(ctx.envdir, "nix")),
         )
     else:
+        raise click.ClickException(f"Example must be a directory: {example_path}")
 
-        composition_file = "composition.nix"
-        composition_path = op.abspath(op.join(ctx.envdir, composition_file))
-        click.echo("\nCopy intial files: ")
-        click.echo("   " + create + "  " + ctx.envdir)
-        os.mkdir(ctx.envdir)
+    nxc_json = {
+        # "composition": composition_file,
+        "default_flavour": default_flavour,
+    }
 
-        click.echo("   " + create + "  " + composition_path)
-        copy_file(
-            example_path, composition_path,
-        )
-
-        copy_tree(
-            NXC_NIX_PATH, op.abspath(op.join(ctx.envdir, "nix")),
-        )
-
-        if not disable_detection:
-            platform_detection(ctx)
-
-        if not default_flavour:
-            # determine default flavour
-            if ctx.platform:
-                default_flavour = ctx.platform.default_flavour
-            else:
-                default_flavour = "nixos-test"
-        click.echo("      default flavour: " + click.style(default_flavour, fg="green"))
-
-        nur_str = ""
-        nur_arg = ""
-        extra_configurations = ""
-        input_nur = ""
-        if nur:
-            extra_configurations = " extraConfigurations"
-            if flake:
-                nur_str = NUR_FLAKE
-                nur_arg = ", nur"
-                input_nur = INPUT_NUR_FLAKE
-            else:
-                nur_str = DEFAULT_NUR
-
-            click.echo("      add nur:" + click.style("done", fg="green"))
-
-        if flake:
-            click.echo("\nCreate files for flake support:")
-
-            t = Template(FLAKE)
-            flake_nix = t.substitute(
-                default_flavour=default_flavour,
-                nur=nur_str,
-                nur_arg=nur_arg,
-                input_nur=input_nur,
-                extra_configurations=extra_configurations,
-            )
-
-            click.echo("   " + create + "  flake.nix")
-            with open(op.abspath(op.join(ctx.envdir, "flake.nix")), "w") as f:
-                f.write(flake_nix)
-
-                click.echo("   " + create + "  default.nix")
-                with open(op.abspath(op.join(ctx.envdir, "default.nix")), "w") as f:
-                    f.write(DEFAULT_COMPAT_FLAKE)
-        else:
-            click.echo("\nCreate default file:")
-            click.echo("   " + create + "  default.nix")
-            t = Template(DEFAULT)
-            default_nix = t.substitute(
-                default_flavour=default_flavour,
-                nur=nur_str,
-                extra_configurations=extra_configurations,
-            )
-            with open(op.abspath(op.join(ctx.envdir, "default.nix")), "w") as f:
-                f.write(default_nix)
-
-        nxc_json = {
-            "composition": composition_file,
-            "default_flavour": default_flavour,
-        }
-        if ctx.platform:
-            nxc_json["platform"] = ctx.platform.name
+    if ctx.platform:
+        nxc_json["platform"] = ctx.platform.name
         nxc_json_str = json.dumps(nxc_json)
 
         ctx.nxc = nxc_json
--- tests/test_nixos_compose.py
+++ tests/test_nixos_compose.py
@@ -12,7 +12,7 @@ def test_build(tmp_path):
     print("created temporary directory", tmp_path)
     run("git init", shell=True, cwd=tmp_path)
 
-    res = run("nxc init --flake ", shell=True, cwd=tmp_path)
+    res = run("nxc init", shell=True, cwd=tmp_path)
     assert not res.returncode
 
     run("git add nxc", shell=True, cwd=tmp_path)
@@ -23,3 +23,17 @@ def test_build(tmp_path):
     # s1 = json.dumps({"built": True, "started": False}, sort_keys=True)
     # j1 = json.load(open(f"{tmp_path}/nxc/state.json"))
     # assert s1 == json.dumps(j1)
+
+
+def test_build_nur(tmp_path):
+    print("created temporary directory", tmp_path)
+    run("git init", shell=True, cwd=tmp_path)
+
+    res = run("nxc init --nur", shell=True, cwd=tmp_path)
+    assert not res.returncode
+
+    run("git add nxc", shell=True, cwd=tmp_path)
+
+    # res = run("nxc build -C composition::nixos-test", shell=True, cwd=tmp_path)
+    res = run("nxc build", shell=True, cwd=tmp_path)
+    assert not res.returncode
