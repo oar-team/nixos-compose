058cbad745f974dbf1a57fd2243e40ff79ed6d35
Olivier Richard <olivier.richard@imag.fr>
2021-10-29 19:49:55 +0200

    add callPackage to help package developpement inside composition tree

--- nix/helpers.nix
+++ nix/helpers.nix
@@ -11,4 +11,16 @@ rec {
 
   # funtion to multiple node based on the same configuration, providing an id to base_conf
   makeMany = base_conf: (makeManyById (id: base_conf));
+
+  # function use to help package development inside composition tree
+  # nix-build -E '(import ./nix/helpers.nix).callPackage ./hello {}'
+  # or
+  # nix build --impure --expr '(import ./nix/helpers.nix).callPackage ./hello {}'
+  callPackage = let
+    input-pkgs = (import (builtins.fetchTarball
+      "https://github.com/edolstra/flake-compat/archive/master.tar.gz") {
+        src = ./..;
+      }).defaultNix.inputs.nixpkgs;
+  in input-pkgs.lib.callPackageWith
+  input-pkgs.outputs.legacyPackages.${builtins.currentSystem};
 }
