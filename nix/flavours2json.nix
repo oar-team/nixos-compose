479ac105e72bf197996c5b31dedc3249fa44fdee
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2022-02-10 13:53:49 +0100

    add derivations for the generation of the json of the flavours
    this will help the generation of the list of flavours:
    
    to build the `flavours.json`:
    
    ```sh
    nix build .#flavoursJson
    ```
    
    to show the user the flavours:
    
    ```sh
    nix run .#showFlavours
    ```
    
    (calls `jq` on the generated json of `flavoursJson`)

--- nix/flavours2json.nix
+++ nix/flavours2json.nix
@@ -1,6 +1,3 @@
-# To use
-# nix-build flavours2json.nix --out-link flavours-link.json && cp -f flavours-link.json flavours.json
-#
 { pkgs ? import <nixpkgs> { } }:
 let
   flavours = import ./flavours.nix;
@@ -9,4 +6,10 @@ let
       pkgs.lib.filterAttrs
       (n: v: n == "name" || n == "description" || n == "image") f;
   in pkgs.lib.mapAttrs (n: v: filter_flavour v) flavours;
-in pkgs.writeText "flavours-link.json" (builtins.toJSON filtered_flavours)
+in rec {
+  flavoursJson =
+    pkgs.writeText "flavours-link.json" (builtins.toJSON filtered_flavours);
+  showFlavours = pkgs.writeScriptBin "showFlavours" ''
+    cat ${flavoursJson} | ${pkgs.jq}/bin/jq
+  '';
+}
