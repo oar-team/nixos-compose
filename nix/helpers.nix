c33a0b87853e8e17bbfd426218837467889d5ad0
Olivier Richard <olivier.richard@imag.fr>
2022-02-22 16:38:30 +0100

    add setup.toml support to allow parametrization (example override src in nur.repos)

--- nix/helpers.nix
+++ nix/helpers.nix
@@ -23,4 +23,34 @@ rec {
       }).defaultNix.inputs.nixpkgs;
   in input-pkgs.lib.callPackageWith
   input-pkgs.outputs.legacyPackages.${builtins.currentSystem};
+
+  # mapListToAttrs (o: {${o}=o+"-2";}) [ "a" "b"] 
+  # { a = "a-2"; b = "b-2"; }
+  mapListToAttrs = op: list:
+    let
+      len = builtins.length list;
+      g = n: s:
+        if n == len then s else g (n + 1) (s // (op (builtins.elemAt list n)));
+    in g 0 { };
+
+  # mapAttrNamesToAttrs (o: {${o}=o+"-2";}) { a = 1; b = 2; }
+  # { a = "a-2"; b = "b-2"; }
+  mapAttrNamesToAttrs = op: attrs: mapListToAttrs op (builtins.attrNames attrs);
+
+  # mapAttrsToAttrs (n: v: {${n+"2"}=1+v;}) { a = 1; b = 2; }
+  # { a2 = 2; b2 = 3; }
+  mapAttrsToAttrs = op: attrs:
+    let
+      list = builtins.attrNames attrs;
+      len = builtins.length list;
+      g = n: s:
+        if n == len then
+          s
+        else
+          let
+            attrName = builtins.elemAt list n;
+            value = attrs.${attrName};
+          in g (n + 1) (s // (op attrName value));
+    in g 0 { };
+
 }
--- nix/lib.nix
+++ nix/lib.nix
@@ -3,7 +3,11 @@ in {
   flavours = import ./flavours.nix;
   compose = import ./compose.nix;
   nur = import ./nur.nix;
+  setup = import ./setup.nix;
   makeManyById = helpers.makeManyById;
   makeMany = helpers.makeManyById;
   callPackage = helpers.callPackage;
+  mapListToAttrs = helpers.mapListToAttrs;
+  mapAttrNamesToAttrs = helpers.mapAttrNamesToAttrs;
+  mapAttrsToAttrs = helpers.mapAttrsToAttrs;
 }
