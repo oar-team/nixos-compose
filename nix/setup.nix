cd9a9fe7eed2ca861093843d9204a70e09db0925
Olivier Richard <olivier.richard@imag.fr>
2022-02-22 16:38:30 +0100

    add setup.toml support to allow parametrization (example override src in nur.repos)

---
+++ nix/setup.nix
@@ -0,0 +1,33 @@
+file:
+{ nur ? null }:
+let
+  setup = builtins.fromTOML (builtins.readFile file);
+
+  helpers = import ./helpers.nix;
+
+  adaptAttr = attrName: value: {
+    ${attrName} = (if (attrName == "src") && (builtins.isString value) then
+      /. + value
+    else
+      value);
+  };
+in {
+  overrides = if builtins.hasAttr "overrides" setup then
+    if (builtins.hasAttr "nur" setup.overrides) && (nur != null) then
+      [
+        (self: super:
+          let
+            overrides = repo:
+              builtins.mapAttrs (name: value:
+                super.nur.repos.${repo}.${name}.overrideAttrs
+                (old: helpers.mapAttrsToAttrs adaptAttr value))
+              setup.overrides.nur.${repo};
+          in helpers.mapAttrNamesToAttrs (repo: {
+            nur.repos.${repo} = super.nur.repos.${repo} // (overrides repo);
+          }) setup.overrides.nur)
+      ]
+    else
+      [ ]
+  else
+    [ ];
+}
