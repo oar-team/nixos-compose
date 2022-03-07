1eb4ba61b72b9a2de4d4e5daf0d68364a13f3a40
Olivier Richard <olivier.richard@imag.fr>
2022-03-07 19:47:45 +0100

    fix http server's ip and deploy url

--- nixos_compose/httpd.py
+++ nixos_compose/httpd.py
@@ -49,7 +49,12 @@ class HTTPDaemon:
         HTTPDaemon.ctx = ctx
         self.httpd = socketserver.ThreadingTCPServer(("", port), HTTPRequestHandler)
         self.port = self.httpd.server_address[1]
-        self.ip = socket.gethostbyname(socket.gethostname())
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # doesn't have to be reachable
+        s.connect(('10.255.255.255', 1))
+        self.ip = s.getsockname()[0]
+
         self.httpd_thread = threading.Thread(
             target=self.httpd.serve_forever, daemon=True
         )
