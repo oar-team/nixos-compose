5c77ec8d93897c4cada2e9cc30f9a3dce1a0052f
Olivier Richard <olivier.richard@imag.fr>
2022-04-11 19:22:48 +0200

    Remove ref to deleted stage-2-cmds.nix

--- nix/flavours/nixos-test-ssh.nix
+++ nix/flavours/nixos-test-ssh.nix
@@ -5,7 +5,7 @@
   module = { config, pkgs, lib, modulesPath, ... }: {
     imports = [
       ./shared/stage-1-cmds.nix
-      ./shared/stage-2-cmds.nix
+      ./shared/nxc.nix
       ./shared/common.nix
     ];
     networking.firewall.enable = false;
