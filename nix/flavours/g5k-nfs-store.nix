ac7d29b870f69e2bda0b938ddc7ddbba7384b9e9
Olivier Richard <olivier.richard@imag.fr>
2022-10-16 21:44:11 +0200

    [g5k-nfs-store] shrink bindfs scope to sudo path. Use bindfs for entire store raises issue with mpi application (not clear issue, perhaps to slow  file (open or related ?) operations)

--- nix/flavours/g5k-nfs-store.nix
+++ nix/flavours/g5k-nfs-store.nix
@@ -27,16 +27,31 @@
     # Required for nfs mount to work in the early of stage-2
     boot.initrd.network.flushBeforeStage2 = false;
 
-    systemd.services.nxc-bindfs = {
+    # Workaround for sudo issue w/ nfs-store owned by other than root
+    # sudo: error in /etc/sudo.conf, line 0 while loading plugin "sudoers_policy"
+    # sudo: /nix/store/phzwlf32mgx2a8xvwz6yxql679rl3jwf-sudo-1.9.10/libexec/sudo/sudoers.so must be owned by uid 0
+    # sudo: fatal error, unable to load plugins
+    # TODO: add test if store is owned by root or other user
+    systemd.services.nxc-bindfs-sudo = {
       before = [ "network.target" ];
       wantedBy = [ "multi-user.target" ];
       serviceConfig.Restart= "always";
       script = ''
-        uid=$(stat -c '%u' /nix/.server-ro-store)
-        gid=$(stat -c '%g' /nix/.server-ro-store)
-        echo "Launch bindfs userid/groudid: $uid/$gid"
-        ${pkgs.bindfs}/bin/bindfs -f --map=$uid/0:@$gid/@0 /nix/store /nix/store
+        echo "Launch bindfs for sudo (its files required to be owned by root)"
+        ${pkgs.bindfs}/bin/bindfs -f --force-user=root --force-group=root ${pkgs.sudo} ${pkgs.sudo}
       '';
     };
+
+    # systemd.services.nxc-bindfs-store = {
+    #   before = [ "network.target" ];
+    #   wantedBy = [ "multi-user.target" ];
+    #   serviceConfig.Restart= "always";
+    #   script = ''
+    #     uid=$(stat -c '%u' /nix/.server-ro-store)
+    #     gid=$(stat -c '%g' /nix/.server-ro-store)
+    #     echo "Launch bindfs userid/groudid: $uid/$gid"
+    #     ${pkgs.bindfs}/bin/bindfs -f --map=$uid/0:@$gid/@0 /nix/store /nix/store
+    #   '';
+    # };
   };
 }
