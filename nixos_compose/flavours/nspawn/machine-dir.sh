2400f03e949c751b6668d334609b7d606916cebb
Olivier Richard <olivier.richard@imag.fr>
2025-03-09 18:05:50 +0100

    [nspawn] support for cgroup-v2 and enhance machine_dir script for stop/start

--- nixos_compose/flavours/nspawn/machine-dir.sh
+++ nixos_compose/flavours/nspawn/machine-dir.sh
@@ -5,12 +5,17 @@ base_dir=/var/lib/machines
 prepare() {
     machine=$1
     toplevel=$2
+    deployment_file=$3
     if [[ -z "$machine" ]]; then
-        echo "machine name required"
+        echo "machine name is required"
         exit 1
     fi
     if [[ -z "$toplevel" ]]; then
-        echo "toplevel path required"
+        echo "toplevel path is required"
+        exit 1
+    fi
+    if [[ -z "$deployment_file" ]]; then
+        echo "deployment is required"
         exit 1
     fi
     machine_dir="$base_dir/$machine"
@@ -29,15 +34,14 @@ prepare() {
     mkdir -p etc/nxc
     echo $machine > etc/nxc/hostname
 
+    echo "cp $deployment_file etc/nxc/deployment.json"
+    cp $deployment_file etc/nxc/deployment.json
+
     ln -s $toplevel/etc/os-release etc/
     ln -s $toplevel/init sbin/
 
     mount --bind -o ro /nix/store $machine_dir/nix/store
 
-    mount -t tmpfs tmpfs $machine_dir/run
-    mkdir -p $machine_dir/run/wrappers
-    mount -t tmpfs -o exec,suid tmpfs $machine_dir/run/wrappers
-    mount -t tmpfs -o exec,mode=777 tmpfs $machine_dir/tmp
 }
 
 remove() {
@@ -52,29 +56,40 @@ remove() {
        echo "Warning $machine_dir does not exist"
        exit 0
     fi
-    umount $machine_dir/nix/store
-    umount $machine_dir/run/wrappers
-    umount $machine_dir/run
-    umount $machine_dir/tmp
+
+    if [ -e "$machine_dir/nix/store" ]; then
+        echo "Umount $machine_dir/nix/store"
+        umount $machine_dir/nix/store
+    fi
+
+    if [ -e "/run/systemd/nspawn/unix-export/$machine" ]; then
+       echo "Mount point for $machine exists, umount now"
+       umount "/run/systemd/nspawn/unix-export/$machine"
+    fi
 
     cd $base_dir
-    chattr -i $machine/var/empty
+    if [ -e "$machine/var/empty" ]; then
+        echo "Change $machine/var/empty attribut to allow its remove"
+        chattr -i $machine/var/empty
+    fi
     rm -rf $machine
 }
 
 # See how we were called.
 case "$1" in
     prepare)
-        prepare "$2" "$3"
+        shift
+        prepare "$@"
     ;;
 
     remove)
-        remove "$2"
+        shift
+        remove "$@"
     ;;
 
     reprepare)
         $0 remove "$2"
-        $0 prepare "$2" "$3"
+        $0 prepare "$2" "$3" "$4"
     ;;
 
     *)
