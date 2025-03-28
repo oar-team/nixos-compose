19b6ce56bec103b6ca0ea30cf030e35f7d8646fc
Olivier Richard <olivier.richard@imag.fr>
2025-03-28 16:37:01 +0100

    [nspawn] fix for cgroup-v2 support

--- nixos_compose/flavours/nspawn/machine-dir.sh
+++ nixos_compose/flavours/nspawn/machine-dir.sh
@@ -5,17 +5,12 @@ base_dir=/var/lib/machines
 prepare() {
     machine=$1
     toplevel=$2
-    deployment_file=$3
     if [[ -z "$machine" ]]; then
-        echo "machine name is required"
+        echo "machine name required"
         exit 1
     fi
     if [[ -z "$toplevel" ]]; then
-        echo "toplevel path is required"
-        exit 1
-    fi
-    if [[ -z "$deployment_file" ]]; then
-        echo "deployment is required"
+        echo "toplevel path required"
         exit 1
     fi
     machine_dir="$base_dir/$machine"
@@ -34,14 +29,10 @@ prepare() {
     mkdir -p etc/nxc
     echo $machine > etc/nxc/hostname
 
-    echo "cp $deployment_file etc/nxc/deployment.json"
-    cp $deployment_file etc/nxc/deployment.json
-
     ln -s $toplevel/etc/os-release etc/
     ln -s $toplevel/init sbin/
 
     mount --bind -o ro /nix/store $machine_dir/nix/store
-
 }
 
 remove() {
@@ -56,40 +47,29 @@ remove() {
        echo "Warning $machine_dir does not exist"
        exit 0
     fi
-
-    if [ -e "$machine_dir/nix/store" ]; then
-        echo "Umount $machine_dir/nix/store"
-        umount $machine_dir/nix/store
-    fi
-
-    if [ -e "/run/systemd/nspawn/unix-export/$machine" ]; then
-       echo "Mount point for $machine exists, umount now"
-       umount "/run/systemd/nspawn/unix-export/$machine"
-    fi
+    umount $machine_dir/nix/store
+    #umount $machine_dir/run/wrappers
+    #umount $machine_dir/run
+    #umount $machine_dir/tmp
 
     cd $base_dir
-    if [ -e "$machine/var/empty" ]; then
-        echo "Change $machine/var/empty attribut to allow its remove"
-        chattr -i $machine/var/empty
-    fi
+    chattr -i $machine/var/empty
     rm -rf $machine
 }
 
 # See how we were called.
 case "$1" in
     prepare)
-        shift
-        prepare "$@"
+        prepare "$2" "$3"
     ;;
 
     remove)
-        shift
-        remove "$@"
+        remove "$2"
     ;;
 
     reprepare)
         $0 remove "$2"
-        $0 prepare "$2" "$3" "$4"
+        $0 prepare "$2" "$3"
     ;;
 
     *)
